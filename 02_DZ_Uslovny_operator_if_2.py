6ecec2549268e2bb56c5f4877b8d1027dd4b7557
Aleksiya <alexiyaobraz@yandex.ru>
2024-05-27 16:06:12 +0700

    Добавили 2-й файл

---
+++ 02_DZ_Uslovny_operator_if_2.py
@@ -0,0 +1,11 @@
+first = 42
+second = 69
+third = 42
+
+
+if first == second and first == third: # условие со строгим оператором более трудное
+    print(3)
+elif first == second or first == third: #
+    print(2)
+else:
+    print(0)
